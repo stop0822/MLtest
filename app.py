315a6f0ab6853d373f59e71cb24a88a823287152
stop0822 <zbvldi123@naver.com>
2022-04-15 15:47:13 +0900

    result page완성!

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
-import os
+import os, pickle
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -12,10 +14,17 @@ def mnist():
     if request.method == 'GET':
         return render_template('mnistform.html')
     else:
-         f = request.files['mnistfile']
+        f = request.files['mnistfile']
         path = os.path.dirname(__file__)+'/upload/'+f.filename
         f.save(path)
-        return ★★★★★
+        img = Image.open(path).convert("L")  #image가 html로 변경
+        img = np.resize(img,(1,784))
+        # img = 255 - (img) #반전
+        mpath = os.path.dirname(__file__)+'/model1.pickle'
+        with open(mpath,'rb') as f:
+            model = pickle.load(f)
+        pred = model.predict(img)
+        return render_template('mnistresult.html',data=pred)
 
 if __name__ == '__main__':
     app.run(debug=True)
