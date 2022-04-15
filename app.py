4a362a84f714526b7f80412b9d5d2d797af2d133
stop0822 <zbvldi123@naver.com>
2022-04-15 15:35:59 +0900

    mnist 숫자인식 form

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/mnist',methods=['GET','POST'])
+def mnist():
+    if request.method == 'GET':
+        return render_template('mnistform.html')
+    else:
+        pass
 
 if __name__ == '__main__':
     app.run(debug=True)
